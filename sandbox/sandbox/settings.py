8a646ab9cd415889e55f2581cd4b4baa8630560e
Sebastian Vetter <sebastian@roadside-developer.com>
2014-05-19 12:09:49 +1000

    Set sandbox DB to use atomic DB operations

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -20,6 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
+        'ATOMIC_REQUESTS': True,
     }
 }
 
